57e8a8e0ba278f99b5e6a3a474081bc8d67b6388
Jerusalem Tut <Jtut@gmu.edu>
2026-04-06 22:01:45 +0000

    cloudfare update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // ============================================
 
 // DOM Elements
+const WORKER_url = "https://dash.cloudflare.com/c1e6ac290436ec97b201679215d78d96/workers/services/edit/loreal-chatbot-worker/production";
 const chatForm = document.getElementById("chatForm");
 const userInput = document.getElementById("userInput");
 const chatWindow = document.getElementById("chatWindow");
